80c6bf31b19d3168a6429e0b203686389f7a74cb
Markus Thömmes <markusthoemmes@me.com>
2019-03-21 11:38:00 +0100

    Add an example patch to show the concept.

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -3,6 +3,10 @@
 serving_dir="vendor/github.com/knative/serving"
 serving_branch="release-0.4"
 
+# Checkout upstream version
 rm -rf $serving_dir
 git clone --depth 1 --branch $serving_branch https://github.com/knative/serving.git $serving_dir
 rm -rf $serving_dir/.git
+
+# Apply all patches
+patch -p1 < vendor-patches/*.patch
